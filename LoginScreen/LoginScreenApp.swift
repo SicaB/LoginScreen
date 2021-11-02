9e5ddd0bc16f6ee135cd0e9da54830683824a94d
Sacha Behrend <sacha.behrend@gmail.com>
2021-11-02 13:26:42 +0100

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Sacha Behrend on 02/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
